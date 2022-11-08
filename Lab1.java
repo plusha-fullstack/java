d9810c386727193956e3be8c9ab3fc2c12859b47
plusha-fullstack <56169574+plusha-fullstack@users.noreply.github.com>
2022-11-08 08:49:09 +0300

    Update Lab1.java

--- Lab1.java
+++ Lab1.java
@@ -21,3 +21,36 @@ class Lab1 {
     }
     }
 }
+
+
+import java.io.*;
+//import java.util.Arrays;
+import java.util.*;
+class Lab1 {
+    public static void main(String[] args) {
+        try {
+            InputStream inputStream = System.in;
+            Reader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String str = bufferedReader.readLine();
+            String digit = bufferedReader.readLine();
+            String[] output = str.split(" ");
+            ArrayList<String> a = new ArrayList<String>(List.of(output));
+            for (int i = 0;i<a.size();i++) {
+                if ((a.get(i)).length() != Integer.parseInt(digit)){
+                    a.remove(i);
+                }
+
+            }
+            String[] b = new String[a.size()];
+            a.toArray(b);
+            Arrays.sort(b);
+            for (int i = 0;i<b.length;i++) {
+                System.out.println(b[i]);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.toString());
+
+        }
+    }
+}
